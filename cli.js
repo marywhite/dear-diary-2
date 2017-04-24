caac0fbc960740cf23a87354daa31cc343b9e1f8
Mary White <mary.white@icfolson.com>
2017-04-24 17:01:34 -0500

    save entries in config, add ability to read

--- cli.js
+++ cli.js
@@ -7,10 +7,16 @@ const {commands} = require('./')
 const cli = meow(`
 	Usage
     $ dear-diary write
-
-	Examples
+    $ dear-diary read
+	
+  Examples
     $ dear-diary write
     $ 💖   ${chalk.yellow('what\'s on your mind angel?')}
+
+    $ dear-diary read
+    $ ${chalk.bold('Tue Feb 14 2017 hating love today')}
+    $ ${chalk.bold('Mon Apr 24 2017 what is love even')}
+    $ ${chalk.bold('Mon Apr 24 2017 thinking of you today')}
 `, {
 })
 
@@ -19,6 +25,7 @@ const [input, ...etc] = cli.input
 try {
   commands[input](etc)
 } catch (err) {
+  console.log(err)
   console.log('i don\'t have anything for that, dear')
   process.exit(1)
 }
--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const prompt = require('prompt')
 const Promise = require('bluebird')
 const chalk = require('chalk')
+const Conf = require('conf')
 
 const _get = Promise.promisify(prompt.get)
+const config = new Conf()
 
 function write() {
   prompt.message = '💖'
@@ -16,6 +18,7 @@ function write() {
 
   return _get({properties})
     .then(result => {
+      config.set(`diary.${Date.now()}`, result.data)
       console.log(chalk.magenta('thanks for sharing love'))
       return result.data
     })
@@ -24,6 +27,22 @@ function write() {
     })
 }
 
+function read() {
+  const diary = config.get('diary')
+  const entries = Object.keys(diary)
+    .map(timestamp => {
+      const writing = diary[timestamp]
+      const d = new Date(Number(timestamp)).toDateString()
+      console.log(chalk.bold(`${d} | ${writing}`))
+      return diary[timestamp]
+    })
+
+  if (entries.length === 0) {
+    console.log(chalk.bold('hmm couldn\'t find anything like that xo'))
+  }
+}
+
 exports.commands = {
-  write
+  write,
+  read
 }
