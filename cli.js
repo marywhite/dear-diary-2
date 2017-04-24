340e9643740766caa1a91d284e3f4a2b4a6815bb
Mary White <mary.white@icfolson.com>
2017-04-24 17:40:56 -0500

    add readme & improve help

--- cli.js
+++ cli.js
@@ -10,25 +10,27 @@ const cli = meow(`
     $ dear-diary read
     $ dear-diary read <keyword>
     $ dear-diary read <DD/MM/YYYY>
-	  $ dear-diary myname <name>
+    $ dear-diary myname <name>
+  
   Examples
     $ dear-diary write
-    $ 💖   ${chalk.yellow('what\'s on your mind angel?')}
+    💖   ${chalk.yellow('what\'s on your mind angel?')}
 
     $ dear-diary read
-    $ ${chalk.bold('Tue Feb 14 2017 hating love today')}
-    $ ${chalk.bold('Mon Apr 24 2017 what is love even')}
-    $ ${chalk.bold('Mon Apr 24 2017 thinking of you today')}
+    ${chalk.bold('Tue Feb 14 2017 hating love today')}
+    ${chalk.bold('Mon Apr 24 2017 what is love even')}
+    ${chalk.bold('Mon Apr 24 2017 thinking of you today')}
 
     $ dear-diary read 04/24/2017
-    $ ${chalk.bold('Mon Apr 24 2017 thinking of you today')}
+    ${chalk.bold('Mon Apr 24 2017 what is love even')}
+    ${chalk.bold('Mon Apr 24 2017 thinking of you today')}
 
     $ dear-diary read love
-    $ ${chalk.bold('Tue Feb 14 2017 hating love today')}
-    $ ${chalk.bold('Mon Apr 24 2017 what is love even')}
+    ${chalk.bold('Tue Feb 14 2017 hating love today')}
+    ${chalk.bold('Mon Apr 24 2017 what is love even')}
 
     $ dear-diary myname jane
-    $ ${chalk.blue('nice to meet you jane')}
+    ${chalk.blue('nice to meet you jane')}
 `, {
 })
 
---
+++ readme.md
@@ -0,0 +1,42 @@
+# dear-diary
+a simple tool for keeping secrets. second implementation ~in progress~
+
+## Usage
+first personalize
+```
+$ dear-diary myname <name>
+nice to meet you <name>
+```
+
+write in your diary
+```
+$ dear-diary write
+💖  what's on your mind today angel?
+```
+
+read it later
+
+to view all entries
+```
+$ dear-diary read
+Tue Feb 14 2017 hating love today
+Mon Apr 24 2017 what is love even
+Mon Apr 24 2017 thinking of you today
+```
+
+to find a certian date
+```
+$ dear-diary read 02/14/2017
+Mon Apr 24 2017 what is love even
+Mon Apr 24 2017 thinking of you today
+```
+
+to find a cerain word 
+```
+$ dear-diary read love
+Tue Feb 14 2017 hating love today
+Mon Apr 24 2017 what is love even
+```
+
+encryprion to come
+marywhite xo
